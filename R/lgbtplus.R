4929d4215aa4a16b3ba8ab4d454276282b0c885f
Homer White <hwhite0@georgetowncollege.edu>
2015-01-11 14:03:55 -0500

    add firesetting data

--- R/lgbtplus.R
+++ R/lgbtplus.R
@@ -6,7 +6,7 @@
 #' @name lgbtplus
 #' @docType data
 #' @format A data frame with 75 observations on the following 22 variables.
-#' \describe{ 
+#' \describe{
 #' \item{Gender}{ }
 #' \item{LGBTPlus}{ }
 #' \item{Age}{ }
@@ -30,14 +30,13 @@
 #' \item{Q10V}{ }
 #' \item{Q11E}{ }
 #' }
-#' @source  Hunter Gatewood, Molly Dixon, Almond Bailey.  Class: PSY 311, Fall 2014.
-#' Instructor:  Dr. Regan Lookadoo. For survey form see \url{../doc/lbgt_likert.pdf}.
-#' @note  From Hunter Gatewood (email): "It was coded mainly based upon the numbers
+#' @source Hunter Gatewood, Molly Dixon, Almond Bailey. Class: PSY 311, Fall 2014.
+#' Instructor: Dr. Regan Lookadoo. For survey form see \url{../doc/lbgt_likert.pdf}.
+#' @note From Hunter Gatewood (email): "It was coded mainly based upon the numbers
 #' stated within the questions. For question 7 (on the front), if they answered no
 #' for question 6 (front), we coded it as 5. For question 11, we had to separate
 #' it between 'for a short time' (1-3 semesters) and 'for a longer time (4+ semesters)
 #' with coding (1,2)."
-
 #' @keywords datasets
 #'
 NA
---
+++ processing/fire_study_munging.R
@@ -0,0 +1,48 @@
+require(foreign)
+
+
+# fac.big <- read.spss("FactorAnalysis_merged_renamed0921.sav")
+names(fac.big)
+grep(names(fac.big),pattern="acade")
+fac.big$acadeCBCL
+fac <- with(fac.big,
+            data.frame(age=age,sex=sex_RC,
+                       race=RaceRC,
+                       school.att=ScaleAttitudeREschool,
+                       academic=AcademicPerf_Scale,
+                       attention.raw=sc6raw_scoredCBCL,
+                       attention.t=sc6t_scoredCBCL,
+                       adhd.raw=dsm4raw_scoredCBCL,
+                       adhd.t=dsm4t_scoredCBCL,
+                       attention=attention,
+                       attention.scaled=sc6t_scored,
+                       adhd=ADHD,
+                       adhd.scaled=dsm4t_scored,
+                       fires=as.factor(q72_RC)
+                      )
+          )
+
+require(plyr)
+
+
+sex <- with(fac,mapvalues(sex,
+                           from=c(0,1),
+                           to=c("female","male")))
+sex <- factor(sex,levels=c("female","male"))
+fac$sex <- sex
+
+race <- with(fac,mapvalues(race,
+                          from=c(1,2,3),
+                          to=c("white","black","other")))
+race<- factor(race,levels=c("white","black","other"))
+fac$race <- race
+
+
+
+firesetting <- data.frame(
+  age=fac$age,sex=fac$sex,race=fac$race,
+  school.attitude=fac$school.att,
+  academic=fac$academic,
+  adhd=fac$adhd.scaled)
+
+save(firesetting,file="../data/firesetting.rda")
