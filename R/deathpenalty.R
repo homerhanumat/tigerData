f8d76a0fbf21c7dff150054439dcd2878053f8b8
Homer White <homerhanumat@gmail.com>
2025-02-13 08:37:04 -0500

    add deathpenalty

---
+++ R/deathpenalty.R
@@ -0,0 +1,20 @@
+#' Race and the Death Penalty
+#'
+#' @description A dataset recreated from summary data that describes
+#' relationships between race of defendant, race of victim, and outcome 
+#' of trial in a number of capital cases in Florida in 1976-1977.
+#'
+#' @name deathpenalty
+#' @docType data
+#' @format A data frame with 326 observations on the following 3 variables.
+#' \describe{ 
+#' \item{defrace}{Race of the defendant in the capital case}
+#' \item{vicrace}{Race of the victim}
+#' \item{death}{Whether or not the defendant in the case received the death penalty}
+#' }
+#' 
+#' @source  Michael J. Radelet: "Racial Characteristics and the Imposition of 
+#' the Death Penalty", \emph{American Sociological Review}, 46 (1981).
+#' @keywords datasets
+#'
+NA
